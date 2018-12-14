99e7d155f7fb378f94a2789ab02b81401d681dc3
zwmm <249462764@qq.com>
2018-12-14 15:03:56 +0800

    脚手架初始化

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="login">
+        登录
+    </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
